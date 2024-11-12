34018ec50f4743bf5d482c67b1730c33958aebc7
RamvigneshA <ramvigneshnanban@gmail.com>
2024-11-12 20:38:16 +0530

    dark and light mode finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import { Button } from "@/components/ui/button"
-import { useState } from 'react'
-import './App.css'
-import Worldmap from "./WorldMap";
+import { Button } from '@/components/ui/button';
+import { Switch } from '@/components/ui/switch';
+import { ThemeProvider } from './theme-provider';
+import { useState } from 'react';
+import './App.css';
+import Worldmap from './WorldMap';
+import { ModeToggle } from './mode-toggle';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="container mx-auto">
-      <Worldmap/>
-    </div>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="container mx-auto ">
+        <div className="container flex justify-between mx-auto justify-center items-center font-mono text-2xl font-light m-10 pl-10 pr-10">
+          <header className="  ">Global Growth Graph</header>
+          {/* <Switch checked={} onCheckedChange={} /> */}
+          <ModeToggle/>
+        </div>
+        <Worldmap />
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
-
-
+export default App;
